9964fe04947608a856a60fd7a69165095467634a
Adrian Abdiji <adrian.abdiji@students.fhnw.ch>
2026-01-13 15:41:40 +0100

    Bug fix bei welchem wenn ein Wort zweimal den gleichen Buchstaben hatte, dieser mehrere male angezeigt wurde.
    hinzufügen von Daten aus einer Liste mit einem Random Wort.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 void main() {
     int count = 0;
-    String toguess = "apfel";                           //zu errratendes Wort
+    String toguess = getword();                         //zu errratendes Wort
     IO.print("Try to guess the 5 letter Word in maximum of 6 tries:\n>");
     String guess = input();                             //der Rateversuch
     if (firstcheck(toguess,guess)){                     // erster check ob es beim ersten versuch schon funktioniert.
@@ -13,6 +13,7 @@ void main() {
         guess = input();
         count++;
     }
+    IO.println("richtiges Wort: "+ toguess);
 }
 String input(){                                         // nimmt einen 5 buchstaben langen Input.
     String guess = IO.readln();                         // soll noch kontrollieren ob der Input ein wort ist
@@ -24,30 +25,34 @@ String input(){                                         // nimmt einen 5 buchsta
     return guess;
 }
 
-
 boolean firstcheck(String right, String guess){         // erster check
     return (right.equals(guess));
 }
 
-
-void lettercheck(String right, String guess){           //kontrolliert jeden buchstaben und gibt ihn in der entsprechenden Farbe zurück
-    for (int i = 0; i < 5; i++){
+void lettercheck(String right, String guess) {           //kontrolliert jeden buchstaben und gibt ihn in der entsprechenden Farbe zurück
+    for (int i = 0; i < 5; i++) {
         String a = String.valueOf(guess.charAt(i));
         if (right.contains(a)){
-            for (int j = 0; j < 5; j++){
-                if (guess.charAt(i) == right.charAt(j)){
-                    if (i == j){
-                        IO.print(Backgroundcolor.ANSI_GREEN+guess.charAt(i)+Backgroundcolor.ANSI_RESET);
-                    }
-                }
-                if (guess.charAt(i) == right.charAt(j)) {
-                    if (i != j){
-                        IO.print(Backgroundcolor.ANSI_YELLOW+guess.charAt(i)+Backgroundcolor.ANSI_RESET);
-
-                    }
-                }
-        }
+            if(guess.charAt(i) == right.charAt(i)){
+                IO.print(Backgroundcolor.ANSI_GREEN + guess.charAt(i) + Backgroundcolor.ANSI_RESET);
+            }else IO.print(Backgroundcolor.ANSI_YELLOW + guess.charAt(i) + Backgroundcolor.ANSI_RESET);
         }else IO.print(guess.charAt(i));
-        }
-    IO.println();
     }
+    IO.println();
+}
+String getword(){//Wort aus der Liste nehmen, damit dieses erraten werden kann.
+    String[] list = new String[264];
+    InputStream is = getClass().getResourceAsStream("/Data/words_de.txt");
+    BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+    for (int i = 0; i < 264; i++){
+        try {
+            list[i] = reader.readLine();
+        } catch (IOException e){
+            throw new RuntimeException(e);
+        }
+    }
+    Random rng = new Random();
+    int a = rng.nextInt(264);
+    String word = list[a];
+    return word;
+}
