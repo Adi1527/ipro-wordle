f4b1cac19dda4b42c9c3a12b2b3429d21b020987
Adrian Abdiji <adrian.abdiji@students.fhnw.ch>
2026-01-14 16:26:08 +0100

    gemeinsam mit Kevin von gunten ein Bugfix vorgenommen beim colorcheck.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 void main() {
     int count = 0;
-    String toguess = getword();                         //zu errratendes Wort
+    String toguess = "waage";                         //zu errratendes Wort
     IO.print("Try to guess the 5 letter Word in maximum of 6 tries:\n>");
     String guess = input();                             //der Rateversuch
     if (firstcheck(toguess,guess)){                     // erster check ob es beim ersten versuch schon funktioniert.
@@ -29,18 +29,68 @@ boolean firstcheck(String right, String guess){         // erster check
     return (right.equals(guess));
 }
 
+//void lettercheck(String right, String guess) {           //kontrolliert jeden buchstaben und gibt ihn in der entsprechenden Farbe zurück
+//    for (int i = 0; i < 5; i++) {
+//        String a = String.valueOf(guess.charAt(i));
+//        if (right.contains(a)){
+//            if(guess.charAt(i) == right.charAt(i)){
+//                IO.print(Backgroundcolor.ANSI_GREEN + guess.charAt(i) + Backgroundcolor.ANSI_RESET);
+//            }else IO.print(Backgroundcolor.ANSI_YELLOW + guess.charAt(i) + Backgroundcolor.ANSI_RESET);
+//        }else IO.print(guess.charAt(i));
+//    }
+//    IO.println();
+//}
+
 void lettercheck(String right, String guess) {           //kontrolliert jeden buchstaben und gibt ihn in der entsprechenden Farbe zurück
+    customString[] first = new customString[5];
     for (int i = 0; i < 5; i++) {
         String a = String.valueOf(guess.charAt(i));
         if (right.contains(a)){
             if(guess.charAt(i) == right.charAt(i)){
-                IO.print(Backgroundcolor.ANSI_GREEN + guess.charAt(i) + Backgroundcolor.ANSI_RESET);
-            }else IO.print(Backgroundcolor.ANSI_YELLOW + guess.charAt(i) + Backgroundcolor.ANSI_RESET);
-        }else IO.print(guess.charAt(i));
+                first[i] = new customString(guess.charAt(i), 3);
+            }else first[i] = new customString(guess.charAt(i), 2);
+        }else first[i] = new customString(guess.charAt(i), 1);
     }
+    printcolor(checkcolor(right,first));
     IO.println();
 }
-String getword(){//Wort aus der Liste nehmen, damit dieses erraten werden kann.
+customString[] checkcolor(String right, customString[] first){
+//    int countright = 0;
+//    for (int i = 0; i < 5; i++){
+//       for (int j = 0; j < 5; j++){
+//           if (right.charAt(i) == right.charAt(j) && i != j){
+//               countright++;
+//           }
+//       }
+//    }
+//    if (countright == 0){
+//        for (int k = 1; k < 5; k++){
+//            for (int f = k; f > 0; f--){
+//                if (first[k].c == first[f].c && k != f){
+//                    first[k].color = 1;
+//                }
+//            }
+//        }
+//    } else {
+//        for (int k = 1; k < 5; k++){
+//            for (int f = k; f > 0; f--){
+//                if (first[k].c == first[f].c){
+//                    first[k] = new customString(first[k].getchar(), 1);
+//                }
+//            }
+//        }
+//    }
+    for (int k = 1; k < 5; k++){                                // in zusammenarbeit mit Kevin von Gunten haben wir den obigen Code verkürzt.
+        for (int f = k - 1; f >= 0; f--){
+            if (first[f].color == 2 && first[k].c == first[f].c && first[k].color == first[f].color){
+                first[k].color = 1;
+            }
+        }
+    }
+    return first;
+}
+
+String getword(){                                       //Wort aus der Liste nehmen, damit dieses erraten werden kann.
     String[] list = new String[264];
     InputStream is = getClass().getResourceAsStream("/Data/words_de.txt");
     BufferedReader reader = new BufferedReader(new InputStreamReader(is));
@@ -55,4 +105,13 @@ String getword(){//Wort aus der Liste nehmen, damit dieses erraten werden kann.
     int a = rng.nextInt(264);
     String word = list[a];
     return word;
+}
+void printcolor(customString[] first){
+    for (int i = 0; i < 5; i++){
+        if (first[i].getint() == 3){
+            IO.print(Backgroundcolor.ANSI_GREEN + first[i].getchar() + Backgroundcolor.ANSI_RESET);
+        } else if (first[i].getint() == 2) {
+            IO.print(Backgroundcolor.ANSI_YELLOW + first[i].getchar() + Backgroundcolor.ANSI_RESET);
+        }else  IO.print(first[i].getchar());
+    }
 }
---
+++ src/customString.java
@@ -0,0 +1,19 @@
+import java.util.Objects;
+
+public class customString {
+    char c;                //speichert den Char welcher wiedergegeben werden muss.
+    int color = 0;              //speichert die Info zur Farbe 1=normal, 2=gelb, 3=grün
+
+    customString(char c, int color){
+        this.c = c;
+        this.color = color;
+    }
+
+    char getchar (){
+        return c;
+    }
+    int getint(){
+        return color;
+    }
+
+}
