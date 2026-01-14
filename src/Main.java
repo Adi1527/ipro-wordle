a87159fa439738e5585086d6e76f9a0ecd6b5ff6
Adrian Abdiji <adrian.abdiji@students.fhnw.ch>
2026-01-14 16:26:58 +0100

    Auskommentierten verbesserten Code entfernt.

--- src/Main.java
+++ src/Main.java
@@ -29,18 +29,6 @@ boolean firstcheck(String right, String guess){         // erster check
     return (right.equals(guess));
 }
 
-//void lettercheck(String right, String guess) {           //kontrolliert jeden buchstaben und gibt ihn in der entsprechenden Farbe zurück
-//    for (int i = 0; i < 5; i++) {
-//        String a = String.valueOf(guess.charAt(i));
-//        if (right.contains(a)){
-//            if(guess.charAt(i) == right.charAt(i)){
-//                IO.print(Backgroundcolor.ANSI_GREEN + guess.charAt(i) + Backgroundcolor.ANSI_RESET);
-//            }else IO.print(Backgroundcolor.ANSI_YELLOW + guess.charAt(i) + Backgroundcolor.ANSI_RESET);
-//        }else IO.print(guess.charAt(i));
-//    }
-//    IO.println();
-//}
-
 void lettercheck(String right, String guess) {           //kontrolliert jeden buchstaben und gibt ihn in der entsprechenden Farbe zurück
     customString[] first = new customString[5];
     for (int i = 0; i < 5; i++) {
@@ -55,31 +43,6 @@ void lettercheck(String right, String guess) {           //kontrolliert jeden bu
     IO.println();
 }
 customString[] checkcolor(String right, customString[] first){
-//    int countright = 0;
-//    for (int i = 0; i < 5; i++){
-//       for (int j = 0; j < 5; j++){
-//           if (right.charAt(i) == right.charAt(j) && i != j){
-//               countright++;
-//           }
-//       }
-//    }
-//    if (countright == 0){
-//        for (int k = 1; k < 5; k++){
-//            for (int f = k; f > 0; f--){
-//                if (first[k].c == first[f].c && k != f){
-//                    first[k].color = 1;
-//                }
-//            }
-//        }
-//    } else {
-//        for (int k = 1; k < 5; k++){
-//            for (int f = k; f > 0; f--){
-//                if (first[k].c == first[f].c){
-//                    first[k] = new customString(first[k].getchar(), 1);
-//                }
-//            }
-//        }
-//    }
     for (int k = 1; k < 5; k++){                                // in zusammenarbeit mit Kevin von Gunten haben wir den obigen Code verkürzt.
         for (int f = k - 1; f >= 0; f--){
             if (first[f].color == 2 && first[k].c == first[f].c && first[k].color == first[f].color){
