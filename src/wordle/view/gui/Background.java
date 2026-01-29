a7634f71f0c3efdf471e96a3a67861daf2819756
Adrian Abdiji <adrian.abdiji@students.fhnw.ch>
2026-01-29 16:40:02 +0100

    Design berechnung noch weniger intensiv für bessere rechenleistung, muss noch eine Lösung finden um dieses noch viel besser zu gestalten

--- src/wordle/view/gui/Background.java
+++ src/wordle/view/gui/Background.java
@@ -15,13 +15,13 @@ public class Background implements Drawable {
         int r = 0;
         int g = 0;
         int b = 0;
-        for (int i = 0; i < 51; i++){
+        for (int i = 0; i < 25; i++){
             gui.setColor(new Color(r,g,b));
             gui.fillCircle(600,200,radius);
-            radius -= 17.25;
-            r += 5;
-            b += 5;
-            g += 5;
+            radius -= 35.2;
+            r += 10;
+            b += 10;
+            g += 10;
         }
     }
 }
