87c5f1cf588d05cbe053b002f934619ecf5e49ae
Adrian Abdiji <adrian.abdiji@students.fhnw.ch>
2026-01-29 15:15:27 +0100

    design changes

--- src/wordle/view/gui/Background.java
+++ src/wordle/view/gui/Background.java
@@ -10,18 +10,13 @@ public class Background implements Drawable {
 
     @Override
     public void draw(Gui gui) {
-        double width = IMG_WIDTH * SCALE;
-        double height = (IMG_HEIGHT + 28) * SCALE;
         double radius = 880;
         int r = 0;
         int g = 0;
         int b = 0;
         for (int i = 0; i < 255; i++){
             gui.setColor(new Color(r,g,b));
-//            gui.fillRect(0,0,width,height);
             gui.fillCircle(600,200,radius);
-            width -= 1;
-            height -= 1;
             radius -= 3.8;
             r += 1;
             b += 1;
